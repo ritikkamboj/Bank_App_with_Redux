05f43e2f28269369641afcc2dcfd8753d8bbe508
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-07-25 12:37:40 +0530

    created the action creators

--- src/Store.js
+++ src/Store.js
@@ -45,30 +45,65 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "banking/credit", payload: 500 });
+// store.dispatch({ type: "banking/credit", payload: 500 });
 
-console.log(store.getState());
+// console.log(store.getState());
 
-store.dispatch({ type: "banking/debit", payload: 200 });
+// store.dispatch({ type: "banking/debit", payload: 200 });
 
-console.log(store.getState());
+// console.log(store.getState());
 
 //case in which we are app;ying for a loan ;
 
-store.dispatch({
-  type: "banking/requestloan",
-  payload: {
-    amount: 1000,
-    loanPurpose: "To Buy A Mercedes Car",
-  },
-});
+// store.dispatch({
+//   type: "banking/requestloan",
+//   payload: {
+//     amount: 1000,
+//     loanPurpose: "To Buy A Mercedes Car",
+//   },
+// });
 
-console.log(store.getState());
+// console.log(store.getState());
 
-// remaining case when we clear the all the loan 
+// remaining case when we clear the all the loan
 
-store.dispatch({type : 'banking/payLoan' })
+// store.dispatch({ type: "banking/payLoan" });
 
-console.log(store.getState());
+// console.log(store.getState());
 
 console.log("the file which contain redux ");
+
+function credit(amount) {
+  return { type: "banking/credit", payload: amount };
+}
+
+function debit(amount) {
+  return { type: "banking/debit", payload: amount };
+}
+
+function requestloan(loan, reason) {
+  return {
+    type: "banking/requestloan",
+    payload: {
+      amount: loan,
+      loanPurpose: reason,
+    },
+  };
+}
+
+function payLoan() {
+  return {type: "banking/payLoan"}
+}
+// all things after that remain same as earlier
+
+store.dispatch(credit(500));
+console.log(store.getState());
+
+store.dispatch(debit(200));
+console.log(store.getState());
+
+store.dispatch(requestloan(1000 , 'Have to buy A Mercedes Car '))
+console.log(store.getState());
+
+store.dispatch(payLoan());
+console.log(store.getState());
